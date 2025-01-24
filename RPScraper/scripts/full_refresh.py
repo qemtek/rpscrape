28a40fb7aa8e0fa7949c6e17c9081904cd9b8744
Christopher Collins <qemtek@gmail.com>
2025-01-24 12:27:39 -0600

    a

--- RPScraper/scripts/full_refresh.py
+++ RPScraper/scripts/full_refresh.py
@@ -35,8 +35,5 @@ for country in countries:
             print(local_file_path)
         try:
             run_rpscrape(country, day)
-            file_name = local_file_path.split('/')[-1]
-            wr.s3.upload(local_file=local_file_path, boto3_session=boto3_session,
-                         path=f"s3://rpscrape/data/{country}/{file_name}")
         except Exception as e:
             print(f"Couldnt get data for {country} on {day}")
